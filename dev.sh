857e650811962459935285bce9eb217b6dd1d604
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-20 13:55:37 +0800

    chore(dev): move dev command to dev.sh script

---
+++ dev.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+check_docker() {
+    command -v docker &>/dev/null && docker info --format '{{.ServerVersion}}' &>/dev/null
+}
+
+if check_docker; then
+    ./vendor/bin/sail ps | grep -q 'Up' || ./vendor/bin/sail up -d
+
+    bunx concurrently \
+        -c "#93c5fd,#c4b5fd,#fb7185" \
+        "./vendor/bin/sail logs --follow" \
+        "./vendor/bin/sail artisan pail --timeout=0" \
+        "./vendor/bin/sail bun run dev" \
+        --names=server,logs,vite \
+        --teardown "./vendor/bin/sail down"
+else
+    concurrently \
+        -c "#93c5fd,#c4b5fd,#fb7185" \
+        "php artisan serve" \
+        "php artisan pail --timeout=0" \
+        "bun run dev" \
+        --names=server,logs,vite
+fi
