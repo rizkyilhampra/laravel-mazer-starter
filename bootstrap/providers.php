d3a385361b622d4c9c7492142dd52f3c261ececc
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-26 15:07:23 +0800

    chore: removed telescope stuff

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,6 +4,5 @@ return [
     App\Providers\AppServiceProvider::class,
     App\Providers\FolioServiceProvider::class,
     App\Providers\FortifyServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
     App\Providers\VoltServiceProvider::class,
 ];
